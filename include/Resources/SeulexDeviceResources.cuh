5c2b16c0e566b6a8daf8c6ecfcbb39cf59226be6
Egor Pavlov <pavlovep@phystech.edu>
2026-07-24 17:14:23 +0300

    update resources

--- include/Resources/SeulexDeviceResources.cuh
+++ include/Resources/SeulexDeviceResources.cuh
@@ -45,113 +45,44 @@ public:
     table() { return table_; }
 
     __device__ scalar* 
-    table(const size_t workIndex) { 
-        return table_ + workIndex * 12 * (this -> sizeOfSystem_); 
-    }
-
-    __device__ scalar* 
     dfdx() { return dfdx_; }
-
-    __device__ scalar* 
-    dfdx(const size_t workIndex) { 
-        return dfdx_ + workIndex * sizeOfSystem_; 
-    }
 
     __device__ scalar* 
     dfdy() { return dfdy_; }
 
     __device__ scalar* 
-    dfdy(const size_t workIndex) { 
-        return dfdy_ + workIndex * sizeOfSystem_ * sizeOfSystem_; 
-    }
-
-    __device__ scalar* 
     a() { return a_; }
-
-    __device__ scalar* 
-    a(const size_t workIndex) { 
-        return a_ + workIndex * sizeOfSystem_ * sizeOfSystem_; 
-    }
 
     __device__ label* 
     pivotIndices() { return pivotIndices_; }
-
-    __device__ label* 
-    pivotIndices(const size_t workIndex) { 
-        return pivotIndices_ + workIndex * sizeOfSystem_; 
-    }
 
     __device__ scalar* 
     dxOpt() { return dxOpt_; }
 
     __device__ scalar* 
-    dxOpt(const size_t workIndex) { 
-        return dxOpt_ + workIndex * sizeOfSystem_; 
-    }
-
-    __device__ scalar* 
     temp() { return temp_; }
-
-    __device__ scalar* 
-    temp(const size_t workIndex) { 
-        return temp_ + workIndex * sizeOfSystem_; 
-    }
 
     __device__ scalar* 
     y0() { return y0_; }
 
     __device__ scalar* 
-    y0(const size_t workIndex) { 
-        return y0_ + workIndex * sizeOfSystem_; 
-    }
-
-    __device__ scalar* 
     ySequence() { return ySequence_; }
-
-    __device__ scalar* 
-    ySequence(const size_t workIndex) { 
-        return ySequence_ + workIndex * sizeOfSystem_; 
-    }
 
     __device__ scalar* 
     scale() { return scale_; }
 
     __device__ scalar* 
-    scale(const size_t workIndex) { 
-        return scale_ + workIndex * sizeOfSystem_; 
-    }
-
-    __device__ scalar* 
     dy() { return dy_; }
-
-    __device__ scalar* 
-    dy(const size_t workIndex) { 
-        return dy_ + workIndex * sizeOfSystem_; 
-    }
 
     __device__ scalar* 
     yTemp() { return yTemp_; }
 
     __device__ scalar* 
-    yTemp(const size_t workIndex) { 
-        return yTemp_ + workIndex * sizeOfSystem_; 
-    }
-
-    __device__ scalar* 
     dydx() { return dydx_; }
-
-    __device__ scalar* 
-    dydx(const size_t workIndex) { 
-        return dydx_ + workIndex * sizeOfSystem_; 
-    }
 
     __device__ scalar* 
     y() { return y_; }
 
-    __device__ scalar* 
-    y(const size_t workIndex) { 
-        return y_ + workIndex * sizeOfSystem_; 
-    }
 };
 
 }
