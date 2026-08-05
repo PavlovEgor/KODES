55e435d76d6de7a37a44f9cbc4f1f0b8bd2336a5
Egor Pavlov <pavlovep@phystech.edu>
2026-08-05 16:55:56 +0300

    update

--- src/Integrator/IntegrationMethods/Seulex.cu
+++ src/Integrator/IntegrationMethods/Seulex.cu
@@ -22,53 +22,55 @@ bool kodes::Seulex<ODESystem>::seul (
     scalar* dydt_  = resources->dydt();
     scalar* y      = resources->ySequence();
 
+    const label systemSize = resources->systemSize();
+
     label nSteps = nSeq_[k];
     scalar dt = dtTot/nSteps;
     
-    for (label i=0; i<resources->systemSize(); i++)
+    for (label i=0; i<systemSize; i++)
     { 
-        for (label j=0; j<resources->systemSize(); j++)
+        for (label j=0; j<systemSize; j++)
         {
-            a_[INDEXMAT(i, j, resources->systemSize())] = -dfdy_[INDEXMAT(i, j, resources->systemSize())];
+            a_[INDEXMAT(i, j, systemSize)] = -dfdy_[INDEXMAT(i, j, systemSize)];
         }
-        a_[INDEXMAT(i, i, resources->systemSize())] += 1/dt;
+        a_[INDEXMAT(i, i, systemSize)] += 1/dt;
     }
     
-    LUDecompose(a_, pivotIndices_, resources->systemSize());
+    LUDecompose(a_, pivotIndices_, systemSize);
 
     scalar xnew = x0 + dt;
     ode->derivatives(xnew, resources->parameters[INDEXVEC(0)], y0_, dy_);
 
-    LUBacksubstitute(a_, pivotIndices_, dy_, resources->systemSize());
+    LUBacksubstitute(a_, pivotIndices_, dy_, systemSize);
 
-    copyVec(yTemp_, y0_, resources->systemSize());
+    copyVec(yTemp_, y0_, systemSize);
 
     for (label nn=1; nn<nSteps; nn++)
     {
-        sumVec(yTemp_, yTemp_, dy_, resources->systemSize());
+        sumVec(yTemp_, yTemp_, dy_, systemSize);
 
         xnew += dt;
 
         if (nn == 1 && k<=1)
         {
             scalar dy1 = 0;
-            for (label i=0; i<resources->systemSize(); i++)
+            for (label i=0; i<systemSize; i++)
             {
                 dy1 += sqr(dy_[INDEXVEC(i)]/scale[INDEXVEC(i)]);
             }
             dy1 = sqrt(dy1);
 
             ode->derivatives(x0 + dt, resources->parameters[INDEXVEC(0)], yTemp_, dydt_);
-            for (label i=0; i<resources->systemSize(); i++)
+            for (label i=0; i<systemSize; i++)
             {
                 dy_[INDEXVEC(i)] = dydt_[INDEXVEC(i)] - dy_[INDEXVEC(i)]/dt;
             }
 
-            LUBacksubstitute(a_, pivotIndices_, dy_, resources->systemSize());
+            LUBacksubstitute(a_, pivotIndices_, dy_, systemSize);
 
             const scalar denom = min(1.0, dy1 + SMALL);
             scalar dy2 = 0;
-            for (label i=0; i<resources->systemSize(); i++)
+            for (label i=0; i<systemSize; i++)
             {
                 // Test of dy_[i] to avoid overflow
                 if (fabs(dy_[INDEXVEC(i)]) > scale[INDEXVEC(i)]*denom)
@@ -89,30 +91,30 @@ bool kodes::Seulex<ODESystem>::seul (
         }
 
         ode->derivatives(xnew, resources->parameters[INDEXVEC(0)], yTemp_, dy_);
-        LUBacksubstitute(a_, pivotIndices_, dy_, resources->systemSize());
+        LUBacksubstitute(a_, pivotIndices_, dy_, systemSize);
     }
 
-    sumVec(y, yTemp_, dy_, resources->systemSize());
+    sumVec(y, yTemp_, dy_, systemSize);
 
     return true;
 }
 
 template<class ODESystem>
 __device__
-void kodes::Seulex<ODESystem>::extrapolate (const label k,const label sizeOfSystem, scalar* table, scalar* y)
+void kodes::Seulex<ODESystem>::extrapolate (const label k,const label systemSize, scalar* table, scalar* y)
 {
     for (label j=k-1; j>0; j--)
     {
-        for (label i=0; i<sizeOfSystem; i++)
+        for (label i=0; i<systemSize; i++)
         {
-            table[INDEXMAT(i, j-1, sizeOfSystem)] =
-                table[INDEXMAT(i, j, sizeOfSystem)] + coeff_[k][j]*(table[INDEXMAT(i, j, sizeOfSystem)] - table[INDEXMAT(i, j-1, sizeOfSystem)]);
+            table[INDEXMAT(i, j-1, systemSize)] =
+                table[INDEXMAT(i, j, systemSize)] + coeff_[k][j]*(table[INDEXMAT(i, j, systemSize)] - table[INDEXMAT(i, j-1, systemSize)]);
         }
     }
 
-    for (label i=0; i<sizeOfSystem; i++)
+    for (label i=0; i<systemSize; i++)
     {
-        y[INDEXVEC(i)] = table[INDEXMAT(i, 0, sizeOfSystem)] + coeff_[k][0]*(table[INDEXMAT(i, 0, sizeOfSystem)] - y[INDEXVEC(i)]);
+        y[INDEXVEC(i)] = table[INDEXMAT(i, 0, systemSize)] + coeff_[k][0]*(table[INDEXMAT(i, 0, systemSize)] - y[INDEXVEC(i)]);
     }
 }
 
@@ -125,9 +127,10 @@ void kodes::Seulex<ODESystem>::step
     kodes::IntegratorControls controls
 )
 {
+    const label systemSize = resources->systemSize();
+
     const scalar absTol_ = controls.absTol;
     const scalar relTol_ = controls.relTol;
-    
 
     const scalar jacRedo_ = min(1e-4, relTol_);
 
@@ -138,7 +141,6 @@ void kodes::Seulex<ODESystem>::step
     scalar* dfdt_  = resources->dfdt();
     scalar* dfdy_  = resources->dfdy();
     
-    
     scalar* dtOpt_ = resources->dtOpt();
     scalar* temp_  = resources->temp();
     scalar* y0_    = resources->y0();
@@ -146,12 +148,11 @@ void kodes::Seulex<ODESystem>::step
     scalar* scale_ = resources->scale();
     
     scalar* y      = resources->vectors;
-
-    scalar& t = resources->currentT[INDEXVEC(0)];
+    scalar& t      = resources->currentT[INDEXVEC(0)];
 
     temp_[INDEXVEC(0)] = GREAT;
     scalar dt = resources->deltaTTry[INDEXVEC(0)];
-    copyVec(y0_, y, resources->systemSize());
+    copyVec(y0_, y, systemSize);
     dtOpt_[INDEXVEC(0)] = fabs(0.1*dt);
 
     if (resources->first[INDEXVEC(0)] || resources->prevReject[INDEXVEC(0)])
@@ -165,7 +166,7 @@ void kodes::Seulex<ODESystem>::step
         kTarg_ = max(1, min(kMaxx_ - 1, label(logTol)));
     }
 
-    for (label i=0; i < resources->systemSize(); ++i)
+    for (label i=0; i < systemSize; ++i)
     {
         scale_[INDEXVEC(i)] = absTol_ + relTol_*fabs(y[INDEXVEC(i)]);
     }
@@ -208,26 +209,26 @@ void kodes::Seulex<ODESystem>::step
 
             if (k == 0)
             {
-                copyVec(y, ySequence_, resources->systemSize());
+                copyVec(y, ySequence_, systemSize);
             }
             else
             {
-                for (label i=0; i<resources->systemSize(); ++i)
+                for (label i=0; i<systemSize; ++i)
                 {
-                    table_[INDEXMAT(i, k-1, resources->systemSize())] = ySequence_[INDEXVEC(i)];
+                    table_[INDEXMAT(i, k-1, systemSize)] = ySequence_[INDEXVEC(i)];
                 }
             }
 
             if (k != 0)
             {
-                extrapolate(k, resources->systemSize(), table_, y);
+                extrapolate(k, systemSize, table_, y);
                 scalar err = 0;
-                for (label i=0; i<resources->systemSize(); ++i)
+                for (label i=0; i<systemSize; ++i)
                 {
                     scale_[INDEXVEC(i)] = absTol_ + relTol_*fabs(y0_[INDEXVEC(i)]);
-                    err += sqr((y[INDEXVEC(i)] - table_[INDEXMAT(i, 0, resources->systemSize())])/scale_[INDEXVEC(i)]);
+                    err += sqr((y[INDEXVEC(i)] - table_[INDEXMAT(i, 0, systemSize)])/scale_[INDEXVEC(i)]);
                 }
-                err = sqrt(err/resources->systemSize());
+                err = sqrt(err/systemSize);
                 if (err > 1/SMALL || (k > 1 && err >= errOld))
                 {
                     resources->reject[INDEXVEC(0)] = true;
