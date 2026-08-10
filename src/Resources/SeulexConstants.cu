c72dfee34ae8f9350cf5b07ad6e858878d70322e
Egor Pavlov <pavlovep@phystech.edu>
2026-08-10 11:43:34 +0300

    correct stepFactors

--- src/Resources/SeulexConstants.cu
+++ src/Resources/SeulexConstants.cu
@@ -1,12 +1,12 @@
 #include "SeulexConstants.cuh"
 
 __constant__ scalar stepFactor1_ = 0.6;
-__constant__ scalar stepFactor2_ = 0.4;
-__constant__ scalar stepFactor3_ = 0.3;
-__constant__ scalar stepFactor4_ = 0.2;
-__constant__ scalar stepFactor5_ = 0.1;
-__constant__ scalar kFactor1_ = 0.5;
-__constant__ scalar kFactor2_ = 0.5;
+__constant__ scalar stepFactor2_ = 0.93;
+__constant__ scalar stepFactor3_ = 0.1;
+__constant__ scalar stepFactor4_ = 4;
+__constant__ scalar stepFactor5_ = 0.5;
+__constant__ scalar kFactor1_ = 0.7;
+__constant__ scalar kFactor2_ = 0.9;
 
 __constant__ label nSeq_[iMaxx_];
 __constant__ scalar gpu_[iMaxx_];
